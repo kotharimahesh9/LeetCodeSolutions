b0628ee4a64b64adb79f4697aeea5587150d7dff
Mahesh Kothari <34913804+kotharimahesh9@users.noreply.github.com>
2024-01-12 12:08:56 -0600

    Time: 2 ms (82.44%), Space: 45.4 MB (31.12%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -8,47 +8,22 @@ class Solution {
         // add the first row and last row
         for(int i=0; i<m; i++){
             if(board[0][i] == 'O'){
-                queue.offer(new Pair(0, i));
-                visited[0][i] = 1;
+                dfs(0, i, n, m, board, visited);
             }
             
             if(board[n-1][i] == 'O'){
-                queue.offer(new Pair(n-1, i));
-                visited[n-1][i] = 1;
+                dfs(n-1, i, n, m, board, visited);
             }
         }
         
         // add the first column and last column
         for(int i=0; i<n; i++){
             if(board[i][0] == 'O'){
-                queue.offer(new Pair(i, 0));
-                visited[i][0] = 1;
+                dfs(i, 0, n, m, board, visited);
             }
             
             if(board[i][m-1] == 'O'){
-                queue.offer(new Pair(i, m-1));
-                visited[i][m-1] = 1;
-            }
-        }
-        
-        int[] dRow = {-1, 0, 1, 0};
-        int[] dCol = {0, -1, 0, 1};
-        while(!queue.isEmpty()){
-            Pair p = queue.poll();
-            int row = p.row;
-            int col = p.col;
-            
-            for(int i=0; i<4; i++){
-                int nextRow = row + dRow[i];
-                int nextCol = col + dCol[i];
-                
-                if(nextRow < n && nextRow >= 0 
-                   && nextCol < m && nextCol >= 0 
-                   && visited[nextRow][nextCol] == 0
-                   && board[nextRow][nextCol] == 'O'){
-                    visited[nextRow][nextCol] = 1;
-                    queue.offer(new Pair(nextRow, nextCol));
-                }
+                dfs(i, m-1, n, m, board, visited);
             }
         }
         
@@ -57,6 +32,25 @@ class Solution {
                 if(visited[i][j] == 0 && board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
+            }
+        }
+        
+    }
+    
+    private void dfs(int row, int col, int n, int m, char[][] board, int[][] visited){
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, -1, 0, 1};
+        
+        visited[row][col] = 1;
+        for(int i=0; i<4; i++){
+            int nextRow = row + dRow[i];
+            int nextCol = col + dCol[i];
+            
+            if(nextRow >=0 && nextRow < n 
+               && nextCol >= 0 && nextCol < m 
+               && board[nextRow][nextCol] == 'O'
+               && visited[nextRow][nextCol] == 0){
+                dfs(nextRow, nextCol, n, m, board, visited);
             }
         }
         
